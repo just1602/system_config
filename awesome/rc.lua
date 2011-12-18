f9876ae49b75202e8961b6ee19bb732931234eb8
Justn Lavoie <jlavoie1602@gmail.com>
2011-12-18 12:43:43 -0500

    change urxvt for urxvtc

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -14,7 +14,7 @@ require("vicious")
 beautiful.init("/home/justin/.config/awesome/theme/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "urxvt"
+terminal = "urxvtc"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
