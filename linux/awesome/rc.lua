2ad8762eb36f689664dc3ec9a0e09e6ec9842813
Justn Lavoie <jlavoie1602@gmail.com>
2016-01-07 12:40:05 -0500

    Change tag style.

--- linux/awesome/rc.lua
+++ linux/awesome/rc.lua
@@ -81,8 +81,8 @@ end
 -- {{{ Tags
 -- Define a tag table which hold all screen tags.
 tags = {}
-tags[1] = awful.tag({ "[www]", "[dev]", "[music]", "[files]", "[foo]" }, 1, layouts[1])
-tags[2] = awful.tag({ "[mail]", "[irc]", "[bar]" }, 2, layouts[1])
+tags[1] = awful.tag({ "www", "dev", "music", "files", "foo" }, 1, layouts[1])
+tags[2] = awful.tag({ "mail", "irc", "bar" }, 2, layouts[1])
 -- for s = 1, screen.count() do
     -- Each screen has its own tag table.
     --  tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
