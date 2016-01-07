f3e9738f681847f4b8937e74018f2e00d1486e38
Justn Lavoie <jlavoie1602@gmail.com>
2016-01-07 15:41:47 -0500

    Add multimedias keys binding.

--- linux/awesome/rc.lua
+++ linux/awesome/rc.lua
@@ -278,7 +278,10 @@ globalkeys = awful.util.table.join(
     -- Multimedia
     awful.key({}, "XF86AudioRaiseVolume", function() awful.util.spawn_with_shell("amixer -q sset Master 3%+") end),
     awful.key({}, "XF86AudioLowerVolume", function() awful.util.spawn_with_shell("amixer -q sset Master 3%-") end),
-    awful.key({}, "XF86AudioMute", function() awful.util.spawn_with_shell("amixer -q sset Master toggle") end)
+    awful.key({}, "XF86AudioMute", function() awful.util.spawn_with_shell("amixer -q sset Master toggle") end),
+    awful.key({}, "XF86AudioPrev", function() awful.util.spawn_with_shell("mpc prev") end),
+    awful.key({}, "XF86AudioPlay", function() awful.util.spawn_with_shell("mpc toggle") end),
+    awful.key({}, "XF86AudioNext", function() awful.util.spawn_with_shell("mpd next") end)
 )
 
 clientkeys = awful.util.table.join(
