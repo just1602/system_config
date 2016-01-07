9b9df98daaa0fe5a20ee5be32ea29c6aafab9f33
Justn Lavoie <jlavoie1602@gmail.com>
2016-01-07 10:54:29 -0500

    Set keyboard layout on startup.

--- linux/awesome/rc.lua
+++ linux/awesome/rc.lua
@@ -447,4 +447,5 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 
 awful.util.spawn_with_shell("google-musicmanager")
+awful.util.spawn_with_shell("setxkbmap -variant fr -layout ca")
 -- }}}
