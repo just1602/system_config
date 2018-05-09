54ea0cad03dbd952034316525d8bf92a7a0adb45
Justin Lavoie <jlavoie1602@gmail.com>
2018-05-08 21:55:32 -0400

    Move the old style .emacs file to init.el file

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -20,18 +20,3 @@
 (require 'magit-settings)
 (require 'neotree-settings)
 (require 'markdown-settings)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (neotree ruby-test-mode markdown-mode web-mode scss-mode rbenv enh-ruby-mode py-autopep8 flycheck elpy magit use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
