ed59cec72cc02f04948d13eb09547ad0c1cc5661
Justin ! <just1602@riseup.net>
2021-04-23 15:03:26 -0400

    Add gopls and go lsp config for nvim

--- roles/nvim/files/nvim/lua/lsp.lua
+++ roles/nvim/files/nvim/lua/lsp.lua
@@ -1,2 +1,3 @@
 require'lspconfig'.solargraph.setup{on_attach=require'completion'.on_attach}
 require'lspconfig'.cssls.setup{on_attach=require'completion'.on_attach}
+require'lspconfig'.gopls.setup{on_attach=require'completion'.on_attach}
