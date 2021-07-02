6646cfd5722607bc7d796c1a396ba9c4498e9309
Justin ! <just1602@riseup.net>
2021-07-02 13:20:16 -0400

    Fix css lsp config

--- roles/nvim/files/nvim/lua/lsp.lua
+++ roles/nvim/files/nvim/lua/lsp.lua
@@ -1,3 +1,6 @@
 require'lspconfig'.solargraph.setup{on_attach=require'completion'.on_attach}
-require'lspconfig'.cssls.setup{on_attach=require'completion'.on_attach}
+require'lspconfig'.cssls.setup{
+    cmd = { 'css-languageserver', '--stdio' },
+    on_attach=require'completion'.on_attach
+}
 require'lspconfig'.gopls.setup{on_attach=require'completion'.on_attach}
