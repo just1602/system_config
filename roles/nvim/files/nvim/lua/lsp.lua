5035f4c526a2f525ca36f38bd9109d9a53d73b55
Justin ! <just1602@riseup.net>
2021-07-12 23:11:34 -0400

    Add the lua-language-server

--- roles/nvim/files/nvim/lua/lsp.lua
+++ roles/nvim/files/nvim/lua/lsp.lua
@@ -1,6 +1,34 @@
-require'lspconfig'.solargraph.setup{on_attach=require'completion'.on_attach}
-require'lspconfig'.cssls.setup{
-    cmd = { 'css-languageserver', '--stdio' },
-    on_attach=require'completion'.on_attach
+require'lspconfig'.solargraph.setup {}
+require'lspconfig'.cssls.setup {
+  cmd = { 'css-languageserver', '--stdio' }
 }
-require'lspconfig'.gopls.setup{on_attach=require'completion'.on_attach}
+require'lspconfig'.gopls.setup {}
+
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+require'lspconfig'.sumneko_lua.setup {
+  cmd = {'/usr/bin/lua-language-server'},
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = runtime_path,
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
